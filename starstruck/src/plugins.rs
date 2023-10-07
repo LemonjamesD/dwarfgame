4a93830c6e548486f6298c55192e735b3bb272f1
LemonjamesD <lemon@lemonjamesd.com>
2023-10-07 12:03:17 -0400

    Made object/plugin run

--- starstruck/src/plugins.rs
+++ starstruck/src/plugins.rs
@@ -3,11 +3,13 @@ use crate::Engine;
 /// A plugin is just a piece of code that runs and interacts with the engine
 pub trait EnginePlugin {
     fn plugin_make(engine: &mut Engine) -> Self;
+    fn plugin_run(&self, engine: &mut Engine) {}
 }
 
 /// A physical object in the game that has state that the engine needs to manage
 pub trait EngineObject<T: Default> {
     fn object_make(engine: &mut Engine, state: T) -> Self;
+    fn object_run(&self, engine: &mut Engine, state: T) {}
 }
 
 /// Called when the engine needs to cleanup an object/plugin
